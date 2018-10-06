f5f15502107339b9e05ceae731a6a94295d6d46b
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-05 23:47:27 -0400

    forgot a couple of comments

--- Ajax/index.php
+++ Ajax/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- a stylesheet from booswatch-->
     <link rel="stylesheet" href="https://bootswatch.com/4/cyborg/bootstrap.min.css">
 <script>
 // The Function to initialize suggestions
