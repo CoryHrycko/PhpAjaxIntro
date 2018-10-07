5bf9391445d2afb6e291c35f1b6ee5bfaa157f18
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-06 20:55:00 -0400

    updating finished oop file

--- oop.php
+++ oop.php
@@ -5,6 +5,8 @@
         // Can set access prefixes
        private $name;
        private $email;
+       // static
+       private static $ageLimit=44;
         //De-constructor
        /*public function __destruct(){
             echo __CLASS__.'destroyed<br>;
@@ -31,10 +33,16 @@
         public function getEmail(){
             return $this->email.'<br>';
     }
+        public static function getAgeLimit(){
+            return self::$ageLimit;
+        }
     }
-
-$person1 = new Person('John Doe','test@test.com');
-echo $person1->getName();
+//for static method
+echo Person :: getAgeLimit();
+//for static props
+echo Person :: $ageLimit;
+//$person1 = new Person('John Doe','test@test.com');
+//echo $person1->getName();
 //$person->name='John Doe';
 
 //echo $person1->name;
@@ -43,8 +51,30 @@ echo $person1->getName();
 //echo $person1->getName();
 
 
+class Customer extends Person{
+    private $balance;
+    public function __construct($name, $email, $balance){
+        parent::__construct($name, $email, $balance);
+        $this->balance = $balance;
+        echo 'A new '.__CLASS__.' has been created: ';
+    }
 
 
+    //set function to protect the class
+    public function setBalance($balance){
+        $this->balance = $balance;
+    }
+    //get function to access the class
+    public function getBalance(){
+        return $this->balance.'<br>';
+    }
+}
+
+
+
+//$customer1 = new Customer('John Doe','test@tes.com', 300);
+
+//echo $customer1 ->getBalance();
 
 
 
