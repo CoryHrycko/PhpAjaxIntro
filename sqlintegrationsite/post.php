6e699ec2ecea925fcabfd8678408d84d6bced1fe
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-07 22:16:42 -0400

    full crud functionality to blog project

--- sqlintegrationsite/post.php
+++ sqlintegrationsite/post.php
@@ -3,6 +3,25 @@
     require('config/config.php');
     //required log in for the server
     require('config/db.php');
+
+    if(isset($_POST['delete'])){
+        //Get form data
+        $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
+       
+    $query = "DELETE FROM posts WHERE id={$delete_id}";
+            //To test query to see if working
+            //die($query)
+        if(mysqli_query($conn, $query)){
+            header('Location: '.ROOT_URL.'');
+        }else{
+            echo "ERROR: ".mysqli_error($conn);
+        }
+
+
+
+
+    }
+
     //get id
     $id = mysqli_real_escape_string($conn, $_GET['id']);
     
@@ -37,8 +56,16 @@ mysqli_close($conn);
         <?php echo $post['author'] ?>
         </small>
         <p><?php echo $post['body'] ?></p>
-</div>
+        <hr>
+        <form class="pull-right" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <input type="hidden" name="delete_id" value="<?php echo $post['id'];?>">
+        <input type="submit"name="delete" value="Delete" class="btn btn-danger">
+        </form>
+
+
+
 <a href="<?php echo ROOT_URL;?>editpost.php?id=<?php echo $post['id'];?>" class="btn btn-default">Edit</a>
+</div>
 <?php include('inc/footer.php'); ?>
 
 
